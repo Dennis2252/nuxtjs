99f5127850d83f1762388bbf4a6083da4e0ca7e3
Dennis2252 <dennisng123456@gmail.com>
2023-09-10 19:31:17 +0700

    Update StarRating.vue

--- components/StarRating.vue
+++ components/StarRating.vue
@@ -3,7 +3,7 @@
         <!-- <button type="button" v-for="i in 10" :class="{ 'mr-1': i < 5 }" v-bind:key="i">
             <svg class="block lg:h-8 md:h-8 h-5 lg:w-8 md:w-8 w-5 bi bi-currency-dollar" :class="[ rating >= i ? 'text-green-800': 'text-gray-600']" fill="currentColor"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/> </svg>
         </button> -->
-        <vue3starRatings :numberOfStars="10" :customSvg="dollarSign" starColor="#85BB65"  class="pr-2" v-model="prop.rating" />
+        <vue3starRatings :numberOfStars="10" :customSvg="dollarSign" :disableClick="true" starColor="#85BB65"  class="pr-2" v-model="prop.rating" />
         <p class="">{{ prop.rating }}</p>
     </div>
     
